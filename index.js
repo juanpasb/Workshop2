4f2c58ae596b4d8cd218f624caa63c044b1cadec
juanpasb <jparomero@unbosque.edu.co>
2021-03-22 14:37:47 -0500

    juanpasb
    cambios

--- index.js
+++ index.js
@@ -12,47 +12,48 @@ function guardar(){
     var fila=("<tr><td>"+_micro+"</td><td>"+_species+"</td><td>"+_Sex+"</td><td>"+_Size+"</td></tr>"+_PotentDangerous+"</td><td>"+_Neighborhood+"</td></tr>");
     var btn = document.createElement("TR");
    	btn.innerHTML=fila;
-    document.getElementById("tabla").appendChild(btn);
+    document.getElementById("BTN").appendChild(btn);
 }
-
-
-
-    var csv = 'csv.json';
-    var request = new XMLHttpRequest();
-    request.open('GET','csv.json', true);
-    request.responseType = 'json';
-    request.send();
-    request.onreadystatechange = function(){
+function cargarDatos(){
+    const data = new XMLHttpRequest();
+    data.open('GET','csvjson',true),
+    data.send();
+    data.onreadystatechange = function(){
         if(this.readyState == 4 && this.status == 200){
             let datos = JSON.parse(this.responseText);
-            let res = document.querySelector('#res');
-            res.innerHTML='';
-            for (let item of datos ) {
-                res.innerHTML +=`
-                <tr>
-                    <td> ${item.microchip} </td>
-                    <td> ${item.species} </td>
-                    <td> ${item.sex} </td>
-                    <td> ${item.size} </td>
-                    <td> ${item.potentDangerous} </td>
-                    <td> ${item.neighborhood} </td>
-                `
-
-            }
+                let res = document.querySelector('#BTN');
+                res.innerHTML='';
+                for (let item of datos ) {
+                    res.innerHTML +=`
+                    <tr>
+                        <td> ${item.microchip} </td>
+                        <td> ${item.species} </td>
+                        <td> ${item.sex} </td>
+                        <td> ${item.size} </td>
+                        <td> ${item.potentDangerous} </td>
+                        <td> ${item.neighborhood} </td>
+                    </tr>
+                    `
+                }
         }
     }
+}
+    
+var btn =  document.querySelector('#boton');
+if(btn)
+{
+    btn.addEventListener("click",cargarDatos);
+}
+
     function RenderizarTabla(dataJsonParaRender) {
-        var tabla = $("#res");
-        var tabla = $("#res").html("");
+        var tabla = $("#tabla");
+        var tabla = $("#tabla").html("");
         $.each(dataParaRender, function (rowIndex, r) {
             var row = $("<tr/>");
-            //Aqui viene el detalle:  
-            //el JSON puede estar desordenado y lo forzamos a trabajar con el orden que definamos
             var myCustomIndex = ["columna1", "columna3", "columna4", "columna8", "columna11"];
             myCustomIndex.forEach(function (index, value) {
                 row.append($("<td/>").text(r[index]));
             });
-            //Ahora que ya tenemos toda nuestra data mostrada en un HTML, procedemos a agregar un boton para alguna operacion
             row.append($("<td/>")
                 .append($('<button/>', {
                     text: 'Seleccionar',
@@ -62,6 +63,23 @@ function guardar(){
                 })));
             tabla.append(row);
         });
+    }
+    function filtrar() {
+        var input, filter, tabla, tr, td, i;
+        input = document.getElementById("myInput");
+        filter = input.value.toUpperCase();
+        table = document.getElementById("BTN");
+        tr = tabla.getElementsByTagName("tr");
+        for (i = 0; j < tr.length; i++) {
+            td = tr[i].getElementsByTagName("td")[0];
+            if (td) {
+                if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+                } else {
+                    tr[i].style.display = "none";
+                }
+            }       
+            }
     }
     
     
